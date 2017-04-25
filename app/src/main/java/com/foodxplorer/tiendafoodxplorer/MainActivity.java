0b0f81f9669d6f2ae7edc3767b5edfb2d07e62a6
ALUMNEDAM <job_sergi@hotmail.es>
2017-04-25 15:27:45 +0200

    Initial commit

---
+++ app/src/main/java/com/foodxplorer/tiendafoodxplorer/MainActivity.java
@@ -0,0 +1,13 @@
+package com.foodxplorer.tiendafoodxplorer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
