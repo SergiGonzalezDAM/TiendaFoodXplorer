aec2678904d350a2c4f574828ae3ff60ded170b9
ALUMNEDAM <job_sergi@hotmail.es>
2017-05-05 16:45:12 +0200

    vista acabada

--- app/src/main/java/com/foodxplorer/tiendafoodxplorer/InfoPedidoActivity.java
+++ app/src/main/java/com/foodxplorer/tiendafoodxplorer/InfoPedidoActivity.java
@@ -2,6 +2,12 @@ package com.foodxplorer.tiendafoodxplorer;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class InfoPedidoActivity extends AppCompatActivity {
 
@@ -9,5 +15,24 @@ public class InfoPedidoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info_pedido);
+
+        List<String> lista = new ArrayList();
+        lista.add("Barbacoa");
+        lista.add("4 Quesos");
+        List<String> listaEj = new ArrayList();
+        listaEj.add("Por hacer");
+        listaEj.add("Listo para entregar");
+        listaEj.add("En reparto");
+        listaEj.add("Entregado");
+
+        ListView listView =(ListView) findViewById(R.id.listViewInfoPedido);
+        Spinner spinner = (Spinner) findViewById(R.id.spinnerEstado);
+
+        ArrayAdapter<String> adaptador = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, lista);
+        ArrayAdapter<String> adaptadorEj = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, listaEj);
+
+        listView.setAdapter(adaptador);
+        spinner.setAdapter(adaptadorEj);
+
     }
 }
